fd3b854a355bbd27996dbdcd8c7d01f2e9a37047
Jen <jfong@mozilla.com>
2016-09-05 14:08:59 -0700

    footer updates

--- public/js/footer.js
+++ public/js/footer.js
@@ -5,11 +5,12 @@
     'glitch.plus',
     'noradio.net'
   ];
+  var cats = ['😹', '😻', '😽'];
 
   network.forEach(function (n) {
     var link = document.createElement('a');
     link.href = 'http://' + n;
-    link.textContent = n;
+    link.textContent = n + ' ' + cats[Math.floor(Math.random() * (cats.length - 1))];
 
     var index = n.indexOf(window.location.origin.split('://')[1]);
     if (index > -1) {
